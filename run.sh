cb33e71fae4ed948370a64455cd1f865fc28aeec
Qiang Li <liqiang@gmail.com>
2017-12-31 20:34:05 -0800

    find ip for X

--- run.sh
+++ run.sh
@@ -10,15 +10,23 @@ set -x
 
 [ -z "$http_proxy" ] && proxy="" || proxy="-e http_proxy=$http_proxy -e https_proxy=$http_proxy -e no_proxy=$no_proxy"
 
+#find local ip for X server
+#required utils
+printf "ifconfig \n awk \n xset \n xhost \n" | xargs -n1 -I{} sh -c 'which {} || exit 255'; if [ $? -ne 0 ]; then
+    exit 1
+fi
 #
-ipaddr=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+ipaddr=$(ifconfig | grep 'inet ' | grep -e '10\.' -e '172\.' -e '192\.'| awk '{$1=$1; print}'|cut -d' ' -f2 | cut -d: -f2 | xargs -n1 -I{} sh -c 'export DISPLAY={}:0; xset q &> /dev/null && echo {} && exit 255;' 2> /dev/null)
 
+export DISPLAY=${ipaddr}:0
 xhost + $ipaddr
 
 #export DHNT_BASE=/c/Users/liqiang/AppData/.dhnt
 [ -z "$DHNT_BASE" ] && export DHNT_BASE=~/.dhnt
-export DISPLAY=${ipaddr}:0
 
-volume="-v /private/tmp:/private/tmp -v ${DHNT_BASE}:/home/vcap"
+#
+volume=""
+[ -d "/private/tmp" ] && volume="$volume -v /private/tmp:/private/tmp"
+[ -d "${DHNT_BASE}" ] && volume="$volume -v ${DHNT_BASE}:/home/vcap"
 
 docker run $proxy $volume -e DISPLAY=${DISPLAY} -it --rm --privileged --name dhnt-idea dhnt/idea
